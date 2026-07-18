e5dee21b1a05473a3d68377c329382cd4bb5f635
22koki <wahomeimani@gmail.com>
2026-07-18 15:31:45 +0300

    Add program model and program listing screen

---
+++ lib/models/program.dart
@@ -0,0 +1,15 @@
+class Program {
+  final String title;
+  final String startDate;
+  final String description;
+  final String eligibility;
+  final String schedule;
+
+  const Program({
+    required this.title,
+    required this.startDate,
+    required this.description,
+    required this.eligibility,
+    required this.schedule,
+  });
+}
